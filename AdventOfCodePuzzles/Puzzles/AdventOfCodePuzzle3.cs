1b06b26697df3f691c4e6c4f1e8299f42d4d1315
Christian Berger <c.berger18@gmail.com>
2021-12-03 09:45:12 +0100

    Day3-Puzzle1

--- AdventOfCodePuzzles/Puzzles/AdventOfCodePuzzle3.cs
+++ AdventOfCodePuzzles/Puzzles/AdventOfCodePuzzle3.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using AdventOfCodePuzzles.Models;
 
 namespace AdventOfCodePuzzles
@@ -7,7 +9,31 @@ namespace AdventOfCodePuzzles
     {
         public override object SolvePuzzle1(IEnumerable<string> input)
         {
-            return null;
+            var inputCount = 0;
+            var length = input.FirstOrDefault().Length;
+            var intFounds = new int[length];
+            
+            foreach (var line in input)
+            {
+                inputCount++;
+                for (int i = 0; i < length; i++)
+                {
+                    var currentNumber = line[i] - '0';
+                    if (currentNumber == 1)
+                        intFounds[i] += currentNumber;
+                }
+            }
+            
+            var binaryRepresentationGamma = string.Empty;
+            for (int i = 0; i < intFounds.Length; i++)
+            {
+                binaryRepresentationGamma += (inputCount / 2 < intFounds[i]) ? 1 : 0;
+            }
+            var maxInt = Math.Pow(2, length) - 1;
+            var gamma = Convert.ToInt32(binaryRepresentationGamma, 2);
+            var epsilon = maxInt - gamma;
+            
+            return gamma*epsilon;
         }
 
         public override object SolvePuzzle2(IEnumerable<string> input)

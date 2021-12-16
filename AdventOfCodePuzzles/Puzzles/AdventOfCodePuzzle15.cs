6a71a4ed74d7bbcb12b7d80c6c5bdaeeafb37e0c
Christian Berger <c.berger18@gmail.com>
2021-12-16 10:29:16 +0100

    Not a good solution but it works - same algorithm, but way bigger dataset (500x500) - takes 5min...

--- AdventOfCodePuzzles/Puzzles/AdventOfCodePuzzle15.cs
+++ AdventOfCodePuzzles/Puzzles/AdventOfCodePuzzle15.cs
@@ -30,7 +30,7 @@ public class AdventOfCodePuzzle15 : PuzzleBase
                 moves.Add(new Point(point.X, point.Y + 1));
 
             if (point.X > 0)
-                 moves.Add(new Point(point.X - 1, point.Y));
+                moves.Add(new Point(point.X - 1, point.Y));
             if (point.Y > 0)
                 moves.Add(new Point(point.X, point.Y - 1));
 
@@ -57,8 +57,9 @@ public class AdventOfCodePuzzle15 : PuzzleBase
         while (shortPQ.Count > 0)
         {
             var point = shortPQ.Dequeue();
-            var moves = possibleMoves(point).Where(x => !visited.ContainsKey(x) || visited[point].Distance + PointValueMap(x) < visited[x].Distance);
-            
+            var moves = possibleMoves(point).Where(x =>
+                !visited.ContainsKey(x) || visited[point].Distance + PointValueMap(x) < visited[x].Distance);
+
             foreach (var move in moves)
             {
                 var accumulatedRisk = visited[point].Distance + PointValueMap(move);
@@ -70,7 +71,7 @@ public class AdventOfCodePuzzle15 : PuzzleBase
                 shortPQ.Enqueue(move, PointValueMap(move) + straightDistanceToEnd(move));
             }
         }
-        
+
         if (visited.TryGetValue(end, out var value))
             return value.Distance;
 
@@ -79,6 +80,88 @@ public class AdventOfCodePuzzle15 : PuzzleBase
 
     public override object SolvePuzzle2(IEnumerable<string> input)
     {
-        throw new NotImplementedException();
+        var square = input.CreateGrid2D(x => x - '0');
+        
+
+        var newSquare = new List<List<int>>();
+        var repeating = 5;
+        for (int y = 0; y < square.Count * repeating; y++)
+        {
+            newSquare.Add(new List<int>());
+            for (int x = 0; x < square[y % square.Count].Count * repeating; x++)
+            {
+                var oldValue = square[y % square.Count][x % square.Count];
+                var newValue = oldValue + x / square.Count + y/square.Count;
+                if (newValue > 9)
+                    newValue -= 9;
+
+                newSquare[^1].Add(newValue);
+            }
+        }
+        
+        var pointToEndMap = new Dictionary<Point, int>();
+        var movesDict = new Dictionary<Point, List<Point>>();
+        var gridYSize = newSquare.Count - 1;
+        var gridXSize = newSquare[0].Count - 1;
+
+        Func<Point, int> PointValueMap = point => newSquare[point.Y][point.X];
+        Func<Point, List<Point>> possibleMoves = point =>
+        {
+            if (movesDict.ContainsKey(point))
+                return movesDict[point];
+
+            var moves = new List<Point>();
+            if (point.X < gridXSize)
+                moves.Add(new Point(point.X + 1, point.Y));
+            if (point.Y < gridYSize)
+                moves.Add(new Point(point.X, point.Y + 1));
+
+            if (point.X > 0)
+                moves.Add(new Point(point.X - 1, point.Y));
+            if (point.Y > 0)
+                moves.Add(new Point(point.X, point.Y - 1));
+
+            movesDict.Add(point, moves);
+            return moves;
+        };
+
+        var start = new Point(0, 0);
+        var end = new Point(gridXSize, gridYSize);
+        var visited = new Dictionary<Point, (int Distance, Point Previous)> {{start, (0, start)}};
+        Func<Point, int> straightDistanceToEnd = (current) =>
+        {
+            if (pointToEndMap.ContainsKey(current))
+                return pointToEndMap[current];
+
+            var dist = Math.Abs(end.X - current.X) + Math.Abs(end.Y - current.Y);
+            pointToEndMap.Add(current, dist);
+            return dist;
+        };
+
+        var shortPQ = new PriorityQueue<Point, int>();
+        shortPQ.Enqueue(start, int.MaxValue);
+
+        while (shortPQ.Count > 0)
+        {
+            var point = shortPQ.Dequeue();
+            var moves = possibleMoves(point).Where(x =>
+                !visited.ContainsKey(x) || visited[point].Distance + PointValueMap(x) < visited[x].Distance);
+
+            foreach (var move in moves)
+            {
+                var accumulatedRisk = visited[point].Distance + PointValueMap(move);
+                if (!visited.ContainsKey(move))
+                    visited.Add(move, (accumulatedRisk, point));
+                else if (accumulatedRisk < visited[move].Distance)
+                    visited[move] = (accumulatedRisk, point);
+
+                shortPQ.Enqueue(move, PointValueMap(move) + straightDistanceToEnd(move));
+            }
+        }
+
+        if (visited.TryGetValue(end, out var value))
+            return value.Distance;
+
+        return "Should not arrive here!";
     }
 }

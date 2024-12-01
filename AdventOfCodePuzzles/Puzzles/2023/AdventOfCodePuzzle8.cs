c917932cf2779a126c96d6d9328302f5350fe02a
Christian Berger <c.berger18@gmail.com>
2024-12-01 11:50:47 +0100

    * Dotnet and Updated Avalonia Version * AoC 2024 - 1

---
+++ AdventOfCodePuzzles/Puzzles/2023/AdventOfCodePuzzle8.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeLib.Attributes;
+using AdventOfCodePuzzles.Models;
+
+namespace AdventOfCodePuzzles.Puzzles._2023
+{
+    [PuzzleType(null, 2023, 8)]
+    public class AdventOfCode2023Puzzle8 : PuzzleBase
+    {
+        public override object SolvePuzzle1(IEnumerable<string> input)
+        {
+            var modifiedInput = input.ToArray();
+            
+            var map = new Dictionary<string, string[]>();
+            var movingInstructionsIndex = modifiedInput[0].Select(x => x == 'L'
+                ? 0
+                : 1).ToArray();
+            
+            foreach (var line in modifiedInput[2..])
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+                
+                var node = line.Split("=", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var position = node[0];
+                var directions = node[1][1..^1].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                map.Add(position, directions);
+            }
+
+            var result = 0;
+            var current = "AAA";
+            while (current != "ZZZ")
+            {
+                var mapPosition = movingInstructionsIndex[result % movingInstructionsIndex.Length];//0 or 1
+                current = map[current][mapPosition];
+                result++;
+            }
+            
+            return result;
+        }
+
+        public override object SolvePuzzle2(IEnumerable<string> input)
+        {
+            var modifiedInput = input.ToArray();
+            
+            var map = new Dictionary<string, string[]>();
+            var validStartingNodes = new HashSet<string>();
+            var movingInstructionsIndex = modifiedInput[0].Select(x => x == 'L'
+                ? 0
+                : 1).ToArray();
+            
+            foreach (var line in modifiedInput[2..])
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+                
+                var node = line.Split("=", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var position = node[0];
+                var directions = node[1][1..^1].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                map.Add(position, directions);
+                if (position[^1] == 'A')
+                    validStartingNodes.Add(position);
+            }
+
+            var result = 0L;
+
+            var processing = validStartingNodes.ToArray();
+            var allEndWithZ = false;
+            while (true)
+            {
+                for (var i = 0; i < processing.Length; i++)
+                {
+                    var currentNode = processing[i];
+                    if (currentNode[^1] != 'Z')
+                        allEndWithZ = false;
+                    
+                    var mapPosition = movingInstructionsIndex[result % movingInstructionsIndex.Length];//0 or 1
+                    var newNode = map[currentNode][mapPosition];
+                    processing[i] = newNode;
+                }
+                
+                if (allEndWithZ)
+                    break;
+
+                result++;
+                allEndWithZ = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCodePuzzles/Puzzles/2024/AdventOfCodePuzzle1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeLib;
+using AdventOfCodeLib.Attributes;
+using AdventOfCodeLib.Extensions;
+using AdventOfCodePuzzles.Models;
+
+namespace AdventOfCodePuzzles.Puzzles._2024
+{
+    [PuzzleType("Puzzle 1", 2024, 1)]
+    public class AdventOfCode2024Puzzle1 : PuzzleBase
+    {
+        public override object SolvePuzzle1(IEnumerable<string> input)
+        {
+            var result = 0L;
+            var leftNumbers = new List<long>();
+            var rightNumbers = new List<long>();
+            var indexFirstSpace = input.First().IndexOf(' ');
+            var indexLastSpace = input.First().LastIndexOf(' ');
+            
+            foreach (var line in input)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                    continue;
+                
+                leftNumbers.Add(line[..indexFirstSpace].ExtractNumber());
+                rightNumbers.Add(line[indexLastSpace..].ExtractNumber());
+            }
+
+            var orderedLeft = leftNumbers.Order().ToList();
+            var orderedRight = rightNumbers.Order().ToList();
+            
+            for (var i = 0; i < leftNumbers.Count; i++)
+            {
+                var difference = Math.Abs(orderedLeft[i] - orderedRight[i]);
+                result += difference;
+            }
+            
+
+            return result;
+        }
+        
+        public override object SolvePuzzle2(IEnumerable<string> input)
+        {
+            var result = 0L;
+            var leftNumbers = new List<long>();
+            var rightNumberCounter = new Dictionary<long, int>();
+            var indexFirstSpace = input.First().IndexOf(' ');
+            var indexLastSpace = input.First().LastIndexOf(' ');
+            
+            foreach (var line in input)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var leftNumber = line[..indexFirstSpace].ExtractNumber();
+                var rightNumber = line[indexLastSpace..].ExtractNumber();
+                
+                leftNumbers.Add(leftNumber);
+                if (!rightNumberCounter.TryAdd(rightNumber, 1))
+                    rightNumberCounter[rightNumber]++;
+            }
+            foreach (var leftNumber in leftNumbers)
+                result += rightNumberCounter.TryGetValue(leftNumber, out var value) ? value * leftNumber : 0;
+
+            return result;
+        }
+    }
+}

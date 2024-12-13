5d4f34f4822241f66c5305820d744b8e2cda47ab
Christian Berger <c.berger18@gmail.com>
2024-12-13 18:25:30 +0100

    Day 4 and Day 5 - Solution 1

---
+++ AdventOfCodePuzzles/Puzzles/2024/AdventOfCodePuzzle4.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCodeLib;
+using AdventOfCodeLib.Attributes;
+using AdventOfCodePuzzles.Models;
+
+namespace AdventOfCodePuzzles.Puzzles._2024
+{
+    [PuzzleType("Puzzle 4", 2024, 4)]
+    public class AdventOfCode2024Puzzle4 : PuzzleBase
+    {
+        public override object SolvePuzzle1(IEnumerable<string> input)
+        {
+            var result = 0;
+            var inputList = input.Where(l => !string.IsNullOrWhiteSpace(l)).ToList();
+
+            var x = inputList[0].Length;
+            var y = inputList.Count;
+            var searchLength = "XMAS".Length;
+            
+            var searchRight = x - 1 + "XMAS".Length < inputList[y].Length;
+            var searchLeft = x + 1 - searchLength >= 0;
+            var searchDown = y - 1 + searchLength < inputList.Count;
+            var searchUp = y + 1 - searchLength >= 0;
+            var searchUpRight = searchUp && searchRight;
+            var searchUpLeft = searchUp && searchLeft;
+            var searchDownRight = searchDown && searchRight;
+            var searchDownLeft = searchDown && searchLeft;
+            
+            for (var i = 0; i < inputList.Count; i++)
+            {
+                for (var j = 0; j < inputList[i].Length; j++)
+                {
+                    if (inputList[i][j] is not 'X') continue;
+                    
+                    result += DiscoverWord("XMAS", inputList, i, j, SearchDirections.SearchDown | SearchDirections.SearchUp | SearchDirections.SearchRight | SearchDirections.SearchLeft);
+                }
+            }
+            return result;
+        }
+        
+        [Flags]
+        private enum SearchDirections : byte
+        {
+            SearchRight,
+            SearchLeft,
+            SearchDown,
+            SearchUp
+        }
+
+        private int DiscoverWord(string searchWord, List<string> inputList, int y, int x, SearchDirections searchDirection)
+        {
+            var count = 0;
+            var searchLength = searchWord.Length;
+
+            var searchRight = searchDirection.HasFlag(SearchDirections.SearchRight);
+            var searchLeft = searchDirection.HasFlag(SearchDirections.SearchLeft);
+            var searchDown = searchDirection.HasFlag(SearchDirections.SearchDown);
+            var searchUp = searchDirection.HasFlag(SearchDirections.SearchUp);
+            var searchUpRight = searchUp && searchRight;
+            var searchUpLeft = searchUp && searchLeft;
+            var searchDownRight = searchDown && searchRight;
+            var searchDownLeft = searchDown && searchLeft;
+            
+            for (var i = 0; i < searchLength; i++)
+            {
+                if (searchRight)
+                    if (inputList[y][x + i] != searchWord[i])
+                        searchRight = false;
+
+                if (searchLeft)
+                    if (inputList[y][x - i] != searchWord[i])
+                        searchLeft = false;
+
+                if (searchDown)
+                    if (inputList[y+i][x] != searchWord[i])
+                        searchDown = false;
+
+                if (searchUp)
+                    if (inputList[y-i][x] != searchWord[i])
+                        searchUp = false;
+                
+                if (searchUpRight)
+                    if (inputList[y-i][x+i] != searchWord[i])
+                        searchUpRight = false;
+                
+                if (searchUpLeft)
+                    if (inputList[y-i][x-i] != searchWord[i])
+                        searchUpLeft = false;
+                
+                if (searchDownRight)
+                    if (inputList[y+i][x+i] != searchWord[i])
+                        searchDownRight = false;
+                
+                if (searchDownLeft)
+                    if (inputList[y+i][x-i] != searchWord[i])
+                        searchDownLeft = false;
+            }
+            
+            if (searchRight)
+                count++;
+            if (searchLeft)
+                count++;
+            if(searchDown)
+                count++;
+            if(searchUp)
+                count++;
+            if(searchUpRight)
+                count++;
+            if(searchUpLeft)
+                count++;
+            if(searchDownRight)
+                count++;
+            if(searchDownLeft)
+                count++;
+            return count;
+        }
+
+        public override object SolvePuzzle2(IEnumerable<string> input)
+        {
+            var result = 0;
+            // var inputList = input.Where(l => !string.IsNullOrWhiteSpace(l)).ToList();
+            // for (var i = 0; i < inputList.Count; i++)
+            // {
+            //     for (var j = 0; j < inputList[i].Length; j++)
+            //     {
+            //         if (inputList[i][j] is 'A')
+            //         {
+            //             DiscoverWord("SAM", inputList, i, j, SearchDirections.SearchDown | SearchDirections.SearchUp);
+            //             DiscoverWord("MAS", inputList, i, j);
+            //         }
+            //         
+            //     }
+            // }
+            return result;
+        }
+    }
+}
---
+++ AdventOfCodePuzzles/Puzzles/2024/AdventOfCodePuzzle5.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using AdventOfCodeLib;
+using AdventOfCodeLib.Attributes;
+using AdventOfCodePuzzles.Models;
+
+namespace AdventOfCodePuzzles.Puzzles._2024
+{
+    [PuzzleType("Puzzle 5", 2024, 5)]
+    public class AdventOfCode2024Puzzle5 : PuzzleBase
+    {
+        public override object SolvePuzzle1(IEnumerable<string> input)
+        {
+            var result = 0L;
+            var inputText = string.Join(Environment.NewLine, input);
+            var sections = inputText.Split(Environment.NewLine + Environment.NewLine);
+            var tmpRules = new List<KeyValuePair<long, long>>();
+
+            var orderingRulesRaw = sections[0]
+                .Split(Environment.NewLine)
+                .Select(l =>
+                    l.Split("|", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+
+            foreach (var orderingRule in orderingRulesRaw)
+            {
+                var firstNumber = orderingRule[0].ExtractNumber();
+                var secondNumber = orderingRule[1].ExtractNumber();
+
+                tmpRules.Add(new KeyValuePair<long, long>(firstNumber, secondNumber));
+            }
+
+
+            var updates = sections[1].Split(Environment.NewLine)
+                .Select(l =>
+                    l.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                        .Select(long.Parse)
+                        .ToList());
+
+            foreach (var update in updates.Where(l => l.Count > 0))
+            {
+                var allBeforeInOrder = true;
+                var allAfterInOrder = true;
+                for (var i = 0; i < update.Count; i++)
+                {
+                    var currentPageNumber = update[i];
+                    Debug.WriteLine("Current Number: " + currentPageNumber);
+                    for (var j = 0; j < i; j++)
+                    {
+                        var allRulesComply = tmpRules //when we have a ruleset for both numbers
+                            .Where(kvp => update.Contains(kvp.Value) && update.Contains(kvp.Key))
+                            .Where(kvp => kvp.Key == currentPageNumber)
+                            .All(k => update.IndexOf(k.Value) >= 0 &&
+                                      update.IndexOf(k.Value) > update.IndexOf(currentPageNumber));
+
+                        allBeforeInOrder &= j == update.Count - 1 || allRulesComply;
+                    }
+
+                    for (var j = i + 1; j < update.Count; j++)
+                    {
+                        var allRulesComply = tmpRules //when we have a ruleset for both numbers
+                            .Where(kvp => update.Contains(kvp.Value) && update.Contains(kvp.Key))
+                            .Where(kvp => kvp.Key == update[j])
+                            .All(k => update.IndexOf(currentPageNumber) >= 0 &&
+                                      update.IndexOf(currentPageNumber) < update.IndexOf(k.Value));
+
+                        allAfterInOrder &= j == update.Count - 1 || allRulesComply;
+                    }
+                }
+
+                if (allBeforeInOrder && allAfterInOrder)
+                    result += GetMiddleNumber(update);
+            }
+
+            return result;
+        }
+
+        private long GetMiddleNumber(IList<long> numbers)
+        {
+            if (numbers.Count % 2 == 0)
+                throw new ArgumentException("Can't get middle number from these numbers");
+
+            return numbers[numbers.Count / 2]; //because its zero based we get the middle this way
+        }
+
+        public override object SolvePuzzle2(IEnumerable<string> input)
+        {
+            var result = 0L;
+
+            return result;
+        }
+    }
+}
